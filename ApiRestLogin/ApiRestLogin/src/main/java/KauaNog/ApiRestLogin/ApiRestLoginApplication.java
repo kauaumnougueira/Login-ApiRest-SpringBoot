ef4407aca221989bb9c88914f3dc99f9ecf04c28
Kauã Nogueira <81389414+kauaumnougueira@users.noreply.github.com>
2022-06-05 00:39:11 -0300

    SpringBoot initializer added

---
+++ ApiRestLogin/ApiRestLogin/src/main/java/KauaNog/ApiRestLogin/ApiRestLoginApplication.java
@@ -0,0 +1,13 @@
+package KauaNog.ApiRestLogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestLoginApplication.class, args);
+	}
+
+}
---
+++ ApiRestLogin/ApiRestLogin/src/test/java/KauaNog/ApiRestLogin/ApiRestLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package KauaNog.ApiRestLogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
